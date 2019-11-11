312e256a51f13e7ac56a1043b69b82fcf3fdeb91
Rory MacGregor <rory.macgregor@astrosat.space>
2019-11-11 13:16:33 +0000

    Removed all references to Blogs.

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,7 +3,6 @@ import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
-import BlogPostPreview from './preview-templates/BlogPostPreview'
 import StoriesPagePreview from './preview-templates/StoriesPagePreview'
 import ModulesPagePreview from './preview-templates/ModulesPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
@@ -15,4 +14,3 @@ CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('stories', StoriesPagePreview)
 CMS.registerPreviewTemplate('modules', ModulesPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
